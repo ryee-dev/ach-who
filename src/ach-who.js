9a7009cc4974d813edbab6f3c9f471a663163605
roger yee <ryee.epicodus@gmail.com>
2018-03-23 13:08:28 -0700

    sets up ui html and input vals

---
+++ src/ach-who.js
@@ -0,0 +1,8 @@
+export class AchWho {
+  constructor() {
+
+  }
+
+  var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
+
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { **insert prototype name** } from './ach-who.js';
+import { AchWho } from './ach-who.js';
 
 $(document).ready(function() {
 
